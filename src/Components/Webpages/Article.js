34a9ba06ae000157d0e6939364c0a49f91eb0c1d
Nathan Cha <nathancha777@gmail.com>
2020-06-18 12:29:42 -0400

    finish make the route for the navbar

---
+++ src/Components/Webpages/Article.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Article () {
+    return(
+        <div>
+            <hi>Article</hi>
+        </div>
+    )
+}
+
+export default Article;
---
+++ src/Components/Webpages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Contact () {
+    return(
+        <div>
+            <hi>Contact</hi>
+        </div>
+    )
+}
+
+export default Contact;
