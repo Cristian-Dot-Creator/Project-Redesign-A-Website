b01c8c9ea0645062d5d43a6a305b8f85baeea411
Ben Flanders <benjamin.flanders96@gmail.com>
2020-06-24 15:41:48 -0400

    uploaded the featured articles section of the home page

--- src/Components/Webpages/Home/Home.js
+++ src/Components/Webpages/Home/Home.js
@@ -4,6 +4,34 @@ function Home () {
     return(
         <div>
             <div className="container">
+                <div className="featured">
+                    <div>
+                        <div className="hero"></div>
+                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h5>AUTHOR AND DATE GO HERE</h5>
+                    </div>
+                    <div>
+                        <p>This is a sample article description that you can pull directly from the website and paste into this paragraph tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
+                    </div> 
+                </div>
+                <div className="headlines">
+                    <div>
+                        <h1>This is something crazy that happened this week! Awesome new Headline!</h1>
+                        <h5>Author Name</h5>
+                    </div>
+                    <div>
+                        <h1>This is something crazy that happened this week! Awesome new Headline!</h1>
+                        <h5>Author Name</h5>
+                    </div>
+                    <div>
+                        <h1>This is something crazy that happened this week! Awesome new Headline!</h1>
+                        <h5>Author Name</h5>
+                    </div>
+                    <div>
+                        <h1>This is something crazy that happened this week! Awesome new Headline!</h1>
+                        <h5>Author Name</h5>
+                    </div>
+                </div>
                 <div className="articles">
                     <div className="card">
                         <div className="image article1"></div>
