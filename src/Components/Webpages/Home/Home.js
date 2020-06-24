660f2ee67b11b32b68612f856c1093bd7b3a4676
Ben Flanders <benjamin.flanders96@gmail.com>
2020-06-24 16:09:43 -0400

    Updated the home page with more articles and refined layout

--- src/Components/Webpages/Home/Home.js
+++ src/Components/Webpages/Home/Home.js
@@ -69,6 +69,36 @@ function Home () {
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
+                </div>
+                <div className="ad-box">
+                    <div className="ad"></div>
+                    <h1>ADVERTISEMENT</h1>
+                </div>
+                <div className="articles">
+                    <div className="card">
+                        <div className="image article1"></div>
+                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h5>AUTHOR AND DATE GO HERE</h5>
+                        <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
+                    </div>
+                    <div className="card">
+                        <div className="image article1"></div>
+                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h5>AUTHOR AND DATE GO HERE</h5>
+                        <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
+                    </div>
+                    <div className="card">
+                        <div className="image article1"></div>
+                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h5>AUTHOR AND DATE GO HERE</h5>
+                        <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
+                    </div>
+                    <div className="card">
+                        <div className="image article1"></div>
+                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h5>AUTHOR AND DATE GO HERE</h5>
+                        <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
+                    </div>
                     <div className="card">
                         <div className="image article1"></div>
                         <h1>ARTICLE TITLE GOES HERE</h1>
