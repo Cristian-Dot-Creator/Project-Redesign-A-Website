09b36560e0faa3c7ef696a22ddee7455b17daaa8
AthertonXiong <xatherton@hotmail.com>
2020-06-25 12:57:59 -0400

    extra crunch

--- src/Components/Webpages/ExtraCrunch/ExtraCrunch.js
+++ src/Components/Webpages/ExtraCrunch/ExtraCrunch.js
@@ -4,7 +4,79 @@ function ExtraCrunch () {
     return(
         <div>
             <hi>ExtraCrunch</hi>
+            <div class="headLine">
+            <div class="headLine-1">Join Extra Crunch</div>
+            <div class="headLine-2">sxasxsaxssheuifheruyfhryuferuyfyreuhfeyufheyu</div>
+            </div>
+        <div class="offer-container">
+            <div class="offers">
+                <button external-event="offer-cta-1" class="card-Container unbutton" ng-click="startCheckout(terms[0].termId)">
+                    <div class="card">
+                        <div class="title">
+                            Monthly Membership
+                        </div>
+                        <div class="period"> Save $80 </div>
+                        <div class="button"> Submit </div>
+                    </div>
+                </button>
+                <button external-event="offer-cta-2" class="card-Container unbutton" ng-click="startCheckout(terms[1].termId)">
+                    <div class="card">
+                        <div class="title">
+                            Monthly Membership
+                        </div>
+                        <div class="period"> Save $80 </div>
+                        <div class="button"> Submit </div>
+                    </div>
+                </button>
+                <button external-event="offer-cta-3" class="card-Container unbutton" ng-click="startCheckout(terms[2].termId)">
+                    <div class="card">
+                        <div class="title">
+                            Monthly Membership
+                        </div>
+                        <div class="period"> Save $80 </div>
+                        <div class="button"> Submit </div>
+                    </div>
+                </button>
+                <div class="benefitContainer">
+                    <div class="flexContainer">
+                        <div class="benefitList">
+                            <div class="benefitTitle">Perk Number 1</div>
+                            <ul>
+                                <li>fksfhdsfhdsjkfhdsafjkasfsd</li>
+                                <li>hjfjdsjffjkefiuehfuiefhuiehfuiae</li>
+                                <li>fheifnejfijfjieojfeiwfheuwifhuaewfa</li>
+                                <li>euifeuifheuifheiufohfoufhufheufiherufh</li>
+                            </ul>
+                        </div>
+                        <div class="benefitList">
+                            <div class="benefitTitle">Perk Number 2</div>
+                            <ul>
+                                <li>fksfhdsfhdsjkfhdsafjkasfsd</li>
+                                <li>hjfjdsjffjkefiuehfuiefhuiehfuiae</li>
+                                <li>fheifnejfijfjieojfeiwfheuwifhuaewfa</li>
+                                <li>euifeuifheuifheiufohfoufhufheufiherufh</li>
+                                <li>fheiufheufheuifhaeiufheiufheifehfeiufiua</li>
+                                <li>ehdewuifherofrefrhufehkfjafufyuafgyeufy</li>
+                            </ul>
+                        </div>
+                        <div class="benefitList">
+                            <div class="benefitTitle">Perk Number 3</div>
+                            <ul>
+                                <li>fksfhdsfhdsjkfhdsafjkasfsd</li>
+                                <li>hjfjdsjffjkefiuehfuiefhuiehfuiae</li>
+                                <li>fheifnejfijfjieojfeiwfheuwifhuaewfa</li>
+                                <li>euifeuifheuifheiufohfoufhufheufiherufh</li>
+                                <li>sfhueirfheorufheruyfhyerufbefeuwfhewauyfhweuy</li>
+                                <li>heiufheofheayfhfbefefbuaefydusgfyusdbfusbf</li>
+                                <li>ksfjiuefhouiehfuifheifdfdskfdisfhueihf</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        </div>
+        
     )
 }
 
