ddae17e481221edd45e62a04b12c41227d25df7d
Ben Flanders <benjamin.flanders96@gmail.com>
2020-06-28 16:15:24 -0400

    Uploaded the working filter and sample articles on the 'ALL ARTICLES' page.

--- src/Components/Webpages/Article/Article.js
+++ src/Components/Webpages/Article/Article.js
@@ -1,124 +1,169 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Article.css';
-function Article () {
+class Article extends Component {
+    render (){
+        setTimeout(() => {
+            articleFilter("all")
+        }, 0)
+        
+        const articleFilter = (c) => {
+            console.log("test")
+          var i;
+          const x = document.getElementsByClassName('cards');
+          const y = Array.from(x)
+          console.log(y)
+          if (c == "all") c = "";
+          for (i = 0; i < y.length; i++) {
+            removeClass(y[i], "show");
+            if (y[i].className.indexOf(c) > -1) addClass(y[i], "show");
+         } 
+        }
+        
+        // Show filtered elements
+        const addClass = (element, name) => {
+          var i, arr1, arr2;
+          arr1 = element.className.split(" ");
+          arr2 = name.split(" ");
+          for (i = 0; i < arr2.length; i++) {
+            if (arr1.indexOf(arr2[i]) == -1) {
+              element.className += " " + arr2[i];
+            }
+          }
+        }
+        
+        // Hide elements that are not selected
+        const removeClass = (element, name) => {
+          var i, arr1, arr2;
+          arr1 = element.className.split(" ");
+          arr2 = name.split(" ");
+          for (i = 0; i < arr2.length; i++) {
+            while (arr1.indexOf(arr2[i]) > -1) {
+              arr1.splice(arr1.indexOf(arr2[i]), 1);
+            }
+          }
+          element.className = arr1.join(" ");
+        }   
+
+
+    
     return(
         <div>
             <div className="container">
                 <h1>ALL ARTICLES</h1>
                 <h5>Filter:</h5>
                 <div id="btnContainer">
-                    <button className="btn active" onClick="articleFilter('all')">Show all</button>
-                    <button className="btn" onClick="articleFilter('political')">Political</button>
-                    <button className="btn" onClick="articleFilter('technology')">Technology</button>
-                    <button className="btn" onClick="articleFilter('business')">Business</button>
-                    <button className="btn" onClick="articleFilter('events')">Events</button>
+                    <button className="button" onClick={() => {articleFilter('all')}}>Show all</button>
+                    <button className="button" onClick={() => {articleFilter('political')}}>Political</button>
+                    <button className="button" onClick={() => {articleFilter('technology')}}>Technology</button>
+                    <button className="button" onClick={() => {articleFilter('business')}}>Business</button>
+                    <button className="button" onClick={() => {articleFilter('events')}}>Events</button>
                 </div>
                 <div className="articles">
-                    <div className="card">
+                    <div className="cards political">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>POLITICAL TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards technology">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>TECHNOLOGY TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards business">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>BUSINESS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards events">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>EVENTS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards political">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>POLITICAL TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards business">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>BUSINESS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards technology">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>TECHNOLOGY TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards events">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>EVENTS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards political">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>POLITICAL TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards technology">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>TECHNOLOGY TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards business">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>BUSINESS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards events">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>EVENTS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards political">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>POLITICAL TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards technology">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>TECHNOLOGY TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards business">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>BUSINESS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards events">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>EVENTS TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards political">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>POLITICAL TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
-                    <div className="card">
+                    <div className="cards technology">
                         <div className="image article1"></div>
-                        <h1>ARTICLE TITLE GOES HERE</h1>
+                        <h1>TECHNOLOGY TITLE GOES HERE</h1>
                         <h5>AUTHOR AND DATE GO HERE</h5>
                         <p>This is a sample article description that you can pull directly from the website and paste into this p tag. This is a sample article description that you can pull directly from the website and paste into this p tag.</p>
                     </div>
@@ -126,6 +171,7 @@ function Article () {
             </div>
         </div>
     )
+    }
 }
 
 export default Article;
